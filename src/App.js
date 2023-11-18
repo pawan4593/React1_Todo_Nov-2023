2fff742e350edc3404b37cda9ab1d94311a2345c
pawan4593 <mr.pawan_patil@yahoo.com>
2023-11-18 18:24:50 +0530

    Created Components such as Todo.js , Header.js , Footer.js , TodoItem.js and added them in the App.js

--- src/App.js
+++ src/App.js
@@ -1,32 +1,16 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Header from './MyComponents/Header';
+import {Todo} from './MyComponents/Todo';
+import {Footer} from './MyComponents/Footer';
 
 function App() {
   
   return (
     <>
-      <nav className="navbar navbar-expand-lg bg-body-tertiary">
-        <div className="container-fluid">
-          <a className="navbar-brand" href="#">Navbar scroll</a>
-          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarScroll" aria-controls="navbarScroll" aria-expanded="false" aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="collapse navbar-collapse" id="navbarScroll">
-            <ul className="navbar-nav me-auto my-2 my-lg-0 navbar-nav-scroll" style={{style : "--bs-scroll-height: 100px;"}}>
-              <li className="nav-item">
-                <a className="nav-link active" aria-current="page" href="#">Home</a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link active" aria-current="page" href="#">About</a>
-              </li>
-            </ul>
-            <form className="d-flex" role="search">
-              <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-              <button className="btn btn-outline-success" type="submit">Search</button>
-            </form>
-          </div>
-        </div>
-      </nav>
+      <Header/>
+      <Todo/>
+      <Footer/>
     </>
   );
 }
---
+++ src/MyComponents/Todo.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { TodoItem } from './TodoItem'
+
+export const Todo = () => {
+  return (
+    <>
+      <TodoItem/>
+      <div>Todo</div>
+    </>
+  )
+}
