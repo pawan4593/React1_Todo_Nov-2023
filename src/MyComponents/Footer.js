36272128ed202049225d863a431c029ac15f3511
pawan4593 <mr.pawan_patil@yahoo.com>
2023-11-19 18:23:37 +0530

    Modified Footer

--- src/MyComponents/Footer.js
+++ src/MyComponents/Footer.js
@@ -1,7 +1,19 @@
 import React from 'react'
 
 export const Footer = () => {
+
+  let footerStyle = {
+    position : "absolute" ,
+    top : "100vh",
+    width : "100%",
+    border : "2px solid red"
+  }
+
   return (
-    <div>Footer</div>
+    <footer className='bg-dark text-light py-3' style={footerStyle}>
+      <p className='text-center'>
+        Copyright &copy; TodosList . All rights reserved .
+      </p>
+    </footer>
   )
 }
--- src/MyComponents/Todo.js
+++ src/MyComponents/Todo.js
@@ -6,12 +6,12 @@ export const Todo = (props) => {
     <>
       <div className="container">
         <h3 className='text-center my-3'>Todos List</h3>
+        {props.todos.length == 0 ? "No todos to display" :""}
         {props.todos.map((todoitem)=>{
           // let sno = todoitem.sno ;
           return (
           <>
             <TodoItem todoitem={todoitem} onDelete={props.onDelete}/>
-            {/* {console.log("todoitem.sno : " + todoitem.sno)} */}
           </>
           )
         })}
